d0d558709b5b76a22e7b0a6ec0ba49fee8c2cd23
AlessioRW <alessiowright@outlook.com>
2023-10-22 16:31:02 +0100

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,90 @@
+import { Audio } from 'expo-av';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Image, Pressable, StyleSheet, View } from 'react-native';
+import sounds from './sounds';
 
 export default function App() {
+  const [sound, setSound] = useState();
+  const [markers, setMarkers] = useState([])
+
+  const strings = [
+    { string: 'e', yMin: 0, yMax: 55 },
+    { string: 'B', yMin: 55, yMax: 90 },
+
+    { string: 'G', yMin: 90, yMax: 130 },
+    { string: 'D', yMin: 130, yMax: 170 },
+    { string: 'A', yMin: 170, yMax: 210 },
+    { string: 'E', yMin: 210, yMax: 250 },
+  ]
+
+  function placeMarker(x, y) {
+    setMarkers(...markers, { x, y })
+    console.log(markers)
+  }
+
+  async function playSound(string, fret) {
+    try {
+      const { sound } = await Audio.Sound.createAsync(sounds['paths'][`${string}-${fret}`])
+      setSound(sound);
+      await sound.playAsync();
+
+    } catch (error) {
+      console.log(`Could not play ${string}-${fret}`)
+    }
+
+
+
+  }
+
+  function onPressFunction(e) {
+
+    const x = e.nativeEvent.locationX
+    const y = e.nativeEvent.locationY
+
+    let string = "-1";
+    let markerPos = {}
+    for (let stringObj of strings) {
+      if (y >= stringObj.yMin && y < stringObj.yMax) {
+        string = stringObj.string
+        markerPos.y = (stringObj.yMin + stringObj.yMax) / 2
+      }
+    }
+    const fret = Math.round(x / 45) - 1
+    markerPos.x = Math.round(x / 45) * 50
+    if (fret < 0 || fret > 15 || string === "-1") {
+      return
+    }
+
+    //console.log(`x: ${x}, y: ${y}`)
+    console.log(`string: ${string}, fret: ${fret}`)
+
+    playSound(string, fret)
+    placeMarker(markerPos.x, markerPos.y)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Pressable onPress={onPressFunction}>
+        <Image
+          source={require('./assets/images/board.png')}
+          style={{
+            height: 250,
+            width: 750,
+          }}
+        />
+        {markers.map((marker) => {
+          <Image
+            source={require('./assets/icon.png')}
+            style={{
+              position: "absolute",
+              x: marker.x,
+              y: marker.y,
+            }}
+          />
+        })}
+      </Pressable>
     </View>
   );
 }
@@ -13,8 +92,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#af0',
     alignItems: 'center',
     justifyContent: 'center',
+    width: "100%",
+    height: "100%"
   },
 });
---
+++ sounds.js
@@ -0,0 +1,34 @@
+
+
+export default sounds = {
+    "paths": {
+        "E-0": require('./assets/audio/E-0.wav'),
+        "E-1": require('./assets/audio/E-1.wav'),
+        "E-2": require('./assets/audio/E-2.wav'),
+        "E-3": require('./assets/audio/E-3.wav'),
+        "E-4": require('./assets/audio/E-4.wav'),
+        "E-5": require('./assets/audio/E-5.wav'),
+        "E-6": require('./assets/audio/E-6.wav'),
+        "E-7": require('./assets/audio/E-7.wav'),
+        "E-8": require('./assets/audio/E-8.wav'),
+        "E-9": require('./assets/audio/E-9.wav'),
+        "E-10": require('./assets/audio/E-10.wav'),
+        "E-11": require('./assets/audio/E-11.wav'),
+        "E-12": require('./assets/audio/E-12.wav'),
+
+        "A-0": require('./assets/audio/A-0.wav'),
+        "A-1": require('./assets/audio/A-1.wav'),
+        "A-2": require('./assets/audio/A-2.wav'),
+        "A-3": require('./assets/audio/A-3.wav'),
+        "A-4": require('./assets/audio/A-4.wav'),
+        "A-5": require('./assets/audio/A-5.wav'),
+        "A-6": require('./assets/audio/A-6.wav'),
+        "A-7": require('./assets/audio/A-7.wav'),
+        "A-8": require('./assets/audio/A-8.wav'),
+        "A-9": require('./assets/audio/A-9.wav'),
+        "A-10": require('./assets/audio/A-10.wav'),
+        "A-11": require('./assets/audio/A-11.wav'),
+        "A-12": require('./assets/audio/A-12.wav'),
+
+    }
+}
